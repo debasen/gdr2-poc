50a23d9187131082dbd1c1fb015141441889be86
ds41755 <ds41755@SWDTPAVM181467.nam.nsroot.net>
2017-12-13 16:07:44 +0530

    Code Changed

--- gdr2-poc/src/com/citi/gdr2/MainClass.java
+++ gdr2-poc/src/com/citi/gdr2/MainClass.java
@@ -1,7 +1,7 @@
 package com.citi.gdr2;
 
 public class MainClass {
-public static void main(String[] args) {
-	
-}
+	public static void main(String[] args) {
+		System.out.println("Code Changed!");
+	}
 }
