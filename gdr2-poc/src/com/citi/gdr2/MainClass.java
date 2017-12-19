f7c38c069b7b28917dadfb5ed7be1d15600c4793
ds41755 <ds41755@SWDTPAVM181467.nam.nsroot.net>
2017-12-19 15:15:33 +0530

    Makes december release related changes

--- gdr2-poc/src/com/citi/gdr2/MainClass.java
+++ gdr2-poc/src/com/citi/gdr2/MainClass.java
@@ -3,5 +3,6 @@ package com.citi.gdr2;
 public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Code Changed!");
+		System.out.println("Making changes for december release");
 	}
 }
