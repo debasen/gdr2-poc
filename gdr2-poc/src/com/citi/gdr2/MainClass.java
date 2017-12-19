c04e061b7cc2f9d7a6e1ea94d2f90d56e2e2c99d
ds41755 <ds41755@SWDTPAVM181467.nam.nsroot.net>
2017-12-19 15:24:40 +0530

    Ready changes

--- gdr2-poc/src/com/citi/gdr2/MainClass.java
+++ gdr2-poc/src/com/citi/gdr2/MainClass.java
@@ -4,5 +4,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		System.out.println("Code Changed!");
 		System.out.println("Making changes for december release");
+		System.out.println("Some ready changes");
 	}
 }
